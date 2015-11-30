cdfe01b5bc9fc56e12694ecc2f419c381854ef30
wenzhang <wenzhang@adobe.com>
2015-11-29 22:32:19 -0800

    fix license, add popup

--- js/index.js
+++ js/index.js
@@ -1,6 +1,60 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
 $(window).load(function() {
   $('a img').responsify();
+  if(readCookie('popupshown') == null && !isMobile.any()){
+	  setTimeout(function(){ 
+	  	$('body').addClass('overlayOn');
+	  }, 45000);
+	  createCookie('popupshown', true, 365);
+  }
+  $('.overlay .close').click(function(){
+  	$('body').removeClass('overlayOn');
+  })
 });
 $(window).resize(function(){
   $('a img').responsify();
 })
+
+// Cookies
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+
+
